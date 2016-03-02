134aabffea200cb6582053f38b7995f52ce0d54a
Tom Wier <t.w.wier@gmail.com>
2016-03-02 13:32:41 -0700

    Adding switched views

--- modules.js
+++ modules.js
@@ -1,11 +1,23 @@
 angular.module('madLibs', [])
 .controller('MadLibsController', function($scope){
-    $scope.female_name = '';
-    $scope.job_title = '';
-    $scope.tedious_task = '';
-    $scope.dirty_task = '';
-    $scope.celebrity = '';
-    $scope.useless_skill = '';
-    $scope.obnoxious_celebrity = '';
-    $scope.huge_number = '';
+    $scope.completed = false;
+
+    $scope.submitWords = function(){
+        console.log($scope.wordsForm.$valid);
+        if($scope.wordsForm.$valid){
+            $scope.completed = true;
+        }
+    }
+
+    $scope.startOver = function(){
+        $scope.completed = false;
+        $scope.female_name = '';
+        $scope.job_title = '';
+        $scope.tedious_task = '';
+        $scope.dirty_task = '';
+        $scope.celebrity = '';
+        $scope.useless_skill = '';
+        $scope.obnoxious_celebrity = '';
+        $scope.huge_number = null;
+    }
 });
