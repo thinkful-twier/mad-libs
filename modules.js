b430be1c783b4fb8b8ed95fdf6a63323e57f78be
Tom Wier <t.w.wier@gmail.com>
2016-03-02 12:58:06 -0700

    adding controller

---
+++ modules.js
@@ -0,0 +1,11 @@
+angular.module('madLibs', [])
+.controller('MadLibsController', function($scope){
+    $scope.female_name = '';
+    $scope.job_title = '';
+    $scope.tedious_task = '';
+    $scope.dirty_task = '';
+    $scope.celebrity = '';
+    $scope.useless_skill = '';
+    $scope.obnoxious_celebrity = '';
+    $scope.huge_number = '';
+});
